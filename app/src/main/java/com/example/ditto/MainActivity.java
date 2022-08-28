8fd73957715373378bea19c666b432f9655fe829
sugheeshan <sugheekalai@gmail.com>
2022-08-28 12:23:55 +0530

    Initial Updation

--- app/src/main/java/com/example/ditto/MainActivity.java
+++ app/src/main/java/com/example/ditto/MainActivity.java
@@ -4,13 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Switch;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView textView,textView2;
     EditText editTextRepeat,repeats;
-    
+    Switch switch1,switch2,switch3;
+    Button button,button2;
+    ImageView imageView;
+    LinearLayout Linear_one;
+    CardView base_card;
 
 
     @Override
